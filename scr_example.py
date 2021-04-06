d7699e03b6e5dc82cdf2da0f4f8ad4c93378faa4
Andrea Patrucco <patrucco@outlook.it>
2021-04-06 11:21:20 +0200

    Added load signal to the example script.

--- scr_example.py
+++ scr_example.py
@@ -9,8 +9,9 @@ alfa.open()
 alfa.initialize()
 # poll a single signal to avoid lag when doing this continuously.
 alfa.poll_signal(12, parse_out = False)
-alfa.add_to_poll_list(12)
-alfa.add_to_poll_list(13)
-alfa.add_to_poll_list(16)
-alfa.add_to_poll_list(17)
+alfa.add_to_poll_list(4)    # Engine Load
+alfa.add_to_poll_list(12)   # Engine RPM
+alfa.add_to_poll_list(13)   # Vehicle Speed [km/h]
+alfa.add_to_poll_list(16)   # MAF [g/s]
+alfa.add_to_poll_list(17)   # Throttle Position
 alfa.poll(parse_out = True, timeout = 10000)
